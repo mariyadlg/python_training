509253b21b2dc40a2341e99b2922e2b564c7a954
marygovory <ereminamv4@gmail.com>
2018-04-17 23:43:10 +0300

    Фикстура

---
+++ application.py
@@ -0,0 +1,91 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def open_add_contact_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_add_contact_page()
+        self.fillFieldWithValue(wd, "firstname", contact.firstname)
+        self.fillFieldWithValue(wd, "middlename", contact.middlename)
+        self.fillFieldWithValue(wd, "lastname", contact.lastname)
+        self.fillFieldWithValue(wd, "nickname", contact.nickname)
+        self.fillFieldWithValue(wd, "title", contact.title)
+        self.fillFieldWithValue(wd, "company", contact.company)
+        self.fillFieldWithValue(wd, "address", contact.address)
+        self.fillFieldWithValue(wd, "home", contact.home)
+        self.fillFieldWithValue(wd, "mobile", contact.mobile)
+        self.fillFieldWithValue(wd, "work", contact.work)
+        self.fillFieldWithValue(wd, "fax", contact.fax)
+        self.fillFieldWithValue(wd, "email", contact.email)
+        self.fillFieldWithValue(wd, "email2", contact.email2)
+        self.fillFieldWithValue(wd, "email3", contact.email3)
+        self.fillFieldWithValue(wd, "homepage", contact.homepage)
+        self.fillFieldWithValue(wd, "address2", contact.address2)
+        self.fillFieldWithValue(wd, "notes", contact.notes)
+        self.fillFieldWithValue(wd, "phone2", contact.phone2)
+
+
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group firm
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+
+    def return_to_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def fillFieldWithValue(self, wd, elementname, value):
+        wd.find_element_by_name(elementname).click()
+        wd.find_element_by_name(elementname).clear()
+        wd.find_element_by_name(elementname).send_keys(value)
+
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
---
+++ test_add_new_contact.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pytest
+from contact import Contact
+from application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    new_contact = Contact(firstname="111", middlename="111", lastname="111", nickname="111", title="111",
+                          company="111", address="111", home="111", mobile="111", work="111", fax="111",
+                          email="111", email2="111", email3="111", homepage="111", address2="111",
+                          notes="111", phone2="111")
+    app.create_contact(new_contact)
+    app.logout()
+
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    new_contact = Contact(firstname="", middlename="", lastname="", nickname="", title="",
+                              company="", address="", home="", mobile="", work="", fax="",
+                              email="", email2="", email3="", homepage="", address2="",
+                              notes="", phone2="")
+    app.create_contact(new_contact)
+    app.logout()
