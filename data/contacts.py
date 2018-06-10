4cf74dbbf11f71502dd1cec2eec2fcccfb42a674
marygovory <ereminamv4@gmail.com>
2018-06-10 17:40:07 +0300

    Реализовала загрузку тестовых данных из файлов

--- data/contacts.py
+++ data/contacts.py
@@ -1,8 +1,6 @@
 from model.contact import Contact
 
 testdata = [
-    Contact(address="address1", firstname="firstname1", lastname="lastname1",
-    all_phones_from_home_page="all_phones,all_email_from_home_page=all_email1"),
-    Contact(address="address2", firstname="firstname2", lastname="lastname2",
-    all_phones_from_home_page="all_phones,all_email_from_home_page=all_email2")
+    Contact(address="address1", firstname="firstname1", lastname="lastname1"),
+    Contact(address="address2", firstname="firstname2", lastname="lastname2")
 ]
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -3,14 +3,16 @@
 from model.contact import Contact
 
 
-def test_add_new_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    sorted_old = sorted(old_contacts, key=Contact.id_or_max)
+    sorted_new = sorted(new_contacts, key=Contact.id_or_max)
+    assert sorted_old == sorted_new
 
 
 
